b7b12ff6b975ec2fdb8fdc148dc5bfd65273e3ca
Ibrahim Duvnjak <duvnjak.ibrahim2004@gmail.com>
2022-12-15 20:25:21 +0100

    made FinalPrice feature appear when +btn is clicked,alse a bit of css

--- New.js
+++ New.js
@@ -1,4 +1,9 @@
 import { menuArray } from "./NewData.js"
+// POINTING DATA FROM DOM OR HTML idk
+const finalPrice = document.querySelector(".finalPrice")
+let priceAll = document.querySelector(".finalPrice p") 
+const buyBtn = document.querySelector(".buyBtn")
+
 
 // DOCUMNET EVENTLISTENERS
 document.addEventListener("click",function(e){
@@ -26,19 +31,30 @@ function renderContent(){
     
 }renderContent()
 
-
+finalPrice.classList.add("hidden")
+buyBtn.classList.add("hidden")
 
 // HANDLE PLUS BTN FUNCTION
 function handlePlusButtons(itemId){{
-
+(function (){
+    finalPrice.classList.remove("hidden")
+    buyBtn.classList.remove("hidden")
+})()
     console.log(typeof itemId,itemId)
 // BASSICLY I COMPARED "itemID" WITH "MenuArray.id" ALSO CALLED
 // "items.id" IN FILTER FUNCTION THEN JUST INNERHTML-ed 
 // task : play with css make it beautiful (a bit),
 // then make it work
+let a = 0
+let b = 0
+let c = 0
     menuArray.filter(function(items){
+        
         if (itemId == items.id){
-            document.querySelector("#order").innerHTML+=`
+            // IF items.id !== DO innerHTML 
+            // and add a print tracker
+            // z.b (a)
+            document.querySelector("#boxes").innerHTML+=`
             <div class="priceList">
             <h1>${items.name} 1x</h1>
             <p>${items.price}$</p>
